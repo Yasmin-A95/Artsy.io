ee797a927b54bbb93a4edb7f07ef2c251d71d175
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-08-12 16:09:45 +1000

    merge

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     console.log("document ready!")
 
+
+
     // basically all our code go in here, you can define a function outside of this but make sure to call it within this
 })
 
