81b2eab7993b4372d7c8d4906c33951a7f822557
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-08-12 11:50:54 +1000

    document ready function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,6 @@
-console.log("is this thing on?")
+$(document).ready(function(){
+    console.log("document ready!")
+
+    // basically all our code go in here, you can define a function outside of this but make sure to call it within this
+})
+
