bc834b0b7ddc8a9ebbae9051ddf11c500baa5794
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-08-15 12:59:28 +1000

    relocated jQuery and deleted an irrelevant js directory

--- js1/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    console.log("document ready!")
-
-
-
-    // basically all our code go in here, you can define a function outside of this but make sure to call it within this
-})
-
