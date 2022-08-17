6b48765cd49e2c68220efe78440b1b1b0787ea35
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-08-18 09:14:56 +1000

    eraser functional

--- static/js/app.js
+++ static/js/app.js
@@ -7,9 +7,20 @@ let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+// resize, 
+// save a copy of the full canvas to the server 
+// then when someone connects it sends that copy of the canvas and initializes with their copy of the canvas 
+// maybe every two hours it refreshes or something
+// every 500 milleseconds it is saved and then new people get that one
+// the server every 500 milleseconds pics one connection at random (or maybe always connection 1 or something idfk how connections are counted,)
+
+// for mobile we need touch down
+
+// int isDrawing
 
 let isDrawing = false;
 
+// handle mouse events
 function handleMouseDown(e) {
 isDrawing = true;
 };
@@ -23,9 +34,33 @@ function handleMouseMove(e) {
     sendMessage(makeMessage(e.offsetX, e.offsetY), connection);
     }
 };
+// touch events for mobile
+
+function handleTouchStart(e) {
+    isDrawing = true;
+};
+
+function handleTouchEnd(e) {
+    isDrawing = false;
+};
+
+function handleTouchMove(e) {
+    if (isDrawing){
+        sendMessage(makeMessage(e.offsetX, e.offsetY))
+    };
+};
+
+// attatch event handlers
 canvas.addEventListener("mousedown", handleMouseDown);
 canvas.addEventListener("mouseup", handleMouseUp);
-canvas.addEventListener("mousemove", handleMouseMove)
+canvas.addEventListener("mousemove", handleMouseMove);
+
+
+// this and the above don't play well together. Interesting. - Debug later
+
+// canvas.addEventListener("touchstart", handleTouchDown);
+// canvas.addEventListener("touchend", handleTouchEnd);
+// canvas.addEventListener("touchmove", handleTouchMove);
 
 connection.onmessage = e => {
     receiveMessages(e, ctx);
--- static/js/functional.js
+++ static/js/functional.js
@@ -1,13 +1,24 @@
 function makeMessage(x, y) {
+    while ($("#tool-select").val() !== "erase") {
+        let message = {
+            tool: $("#tool-select").val(),
+            x: x,
+            y: y,
+            width: $("#width").val(),
+            color: $("#color").val(),
+        }
+        return message;
+    } 
     let message = {
-        tool: "line",
+        tool: $("#tool-select").val(),
         x: x,
         y: y,
         width: $("#width").val(),
-        color: $("#color").val(),
+        color: "#ffffff",
     }
     return message;
 };
+
 
 function receiveMessages(messageEvent, ctx) {
     let message = JSON.parse(messageEvent.data);
@@ -22,9 +33,14 @@ function sendMessage(message, connection) {
 
 function draw (message, ctx) {
     if (message.tool === "line") {
-    drawLine(message, ctx);
-    };
-}
+    drawLine(message, ctx)
+    console.log('drawLine is the thing')
+    } else if (message.tool === "spray") {
+        sprayPaint(message, ctx)
+    } else if (message.tool === "erase") {
+        erase(message, ctx)
+    }
+};
 
 function drawLine(message, ctx) {
     ctx.beginPath();
@@ -35,5 +51,37 @@ function drawLine(message, ctx) {
     ctx.moveTo(message.lastX, message.lastY);
     ctx.lineTo(message.x, message.y);
     ctx.stroke();
+};
+
+function sprayPaint (message, ctx) {
+    console.log('graffiti is illegal');
+    // have it be like line except instead of a block space its a radius where pixels are selected at random within said radius
+    // make a radius based on width
+    // dots
+    let radius = (message.width/2);
+    ctx.fillStyle = message.color;
+    for (let i = 0; i < 30; i++) {
+        let offset = randomPointInRadius(radius);
+        ctx.fillRect(message.x + offset.x, message.y + offset.y, 1, 1);
+    }
+};
+
+function randomPointInRadius(radius) {
+    while (true) { // what the fuck is this
+        var x = Math.random() * 2 - 1;
+        var y = Math.random() * 2 - 1;
+        if (x * x + y * y <= 1)
+            return {x: x * radius, y: y * radius};
+    }
 }
 
+function erase(message, ctx) {
+    ctx.beginPath();
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+    ctx.lineWidth = message.width;
+    ctx.strokeStyle = message.color;
+    ctx.moveTo(message.lastX, message.lastY);
+    ctx.lineTo(message.x, message.y);
+    ctx.stroke();
+}
